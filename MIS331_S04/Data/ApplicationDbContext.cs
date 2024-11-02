b43e06a9fc80c34018ed80567f898587fdb5ee7c
renece153 <87631856+renece153@users.noreply.github.com>
2024-11-02 14:49:17 -0700

    Add project files.

---
+++ MIS331_S04/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MIS331_S04.Data
+{
+    public class ApplicationDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public ApplicationDbContext(DbContextOptions <ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ MIS331_S04/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using MIS331_S04.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(
+        options => options.UseNpgsql(
+            builder.Configuration.GetConnectionString("DefaultConnection")
+            )
+        );
+// Microsoft.AspNetCore.DataProtection.IDataProtectionBuilder dataProtectionBuilder = builder.Services.AddDataProtection(options => options.UseNPGSQL());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
